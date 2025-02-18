fefd64a735d6c6b2f96876d6a32a221b4cba9575
Ranveer225 <singr12@farmingdale.edu>
2025-02-18 09:17:11 -0500

    update index styling

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     console.log("🚀 Algomatics Frontend Loaded!");
+
+    document.getElementById("plotButton").onclick = function() {
+        let equation = document.getElementById("InputEquation").value;
+        document.getElementById("DisplayEquation").innerText = "Equation " + equation;
+    };
 });
